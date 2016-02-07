1e9742a5eea4495f1c049426fccdedbea6e27489
Carl Bradbury <carl@carlbradbury.com>
2016-02-07 19:10:56 +0000

    Added express and underscore packages

--- NodeMovieList/server.js
+++ NodeMovieList/server.js
@@ -1,6 +1,19 @@
 ﻿var http = require('http');
 var port = process.env.port || 1337;
+var msgs = require('./msgs');
+var logger = require('./logger');
+var _ = require("underscore");
+
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    
+    res.writeHead(200, { 'Content-Type': 'text/plain' });   
+
+ 
+    
+    res.write(msgs());
+    
     res.end('Hello World\n');
+
 }).listen(port);
+
+logger.log("This is from the logger");
